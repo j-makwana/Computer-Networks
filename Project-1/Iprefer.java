8f1e7f9b5340ff878d921851bba1bcb7b8b8a830
Jenil Makwana <jenil@vm-instunix-09.cs.wisc.edu>
2025-09-18 12:38:08 -0500

    Iprefer done

--- Project-1/Iprefer.java
+++ Project-1/Iprefer.java
@@ -1,3 +1,8 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Iprefer {
 
@@ -28,6 +33,50 @@ public class Iprefer {
         /////////////////// SOCKET PROGRAMMING////////////////////////
         // based on whether this is a client prgram/ server program we gotta initalize
         /////////////////// things
+
+        if (state == State.SERVER) {
+            // we have the port number
+            int totalBytesRead = 0;
+            try (
+                    ServerSocket serverSocket = new ServerSocket(server_port);
+                    Socket clientSocket = serverSocket.accept();
+                    InputStream in = clientSocket.getInputStream();
+            // lets start a timer here
+            ) {
+                long startTime = System.currentTimeMillis();
+                while (in.read() != -1) {
+                    byte[] data = new byte[1000];
+                    totalBytesRead += in.read(data);
+                }
+                long endTime = System.currentTimeMillis();
+                long duration = endTime - startTime;
+                double rateMbps = (totalBytesRead * 8.0) / (duration / 1000.0) / 1000000.0; // Convert to Mbps
+                System.out.println("received=" + totalBytesRead + " rate=" + String.format("%.2f", rateMbps) + " Mbps");
+            } catch (IOException e) {
+                System.out.println("Exception caught when trying to listen on port "
+                        + server_port + " or listening for a connection");
+                System.out.println(e.getMessage());
+            }
+
+        } else if (state == State.CLIENT) {
+            try (
+                    Socket client = new Socket(hostname, server_port);
+                    OutputStream out = client.getOutputStream();
+
+            ) {
+                byte[] data = new byte[1000];
+                long startTime = System.currentTimeMillis();
+                long endTime = startTime + timeInSeconds * 1000;
+                while (System.currentTimeMillis() < endTime) {
+                    out.write(data);
+                }
+
+            } catch (IOException e) {
+                System.out.println("Exception caught when trying to connect to "
+                        + hostname + " on port " + server_port);
+                System.out.println(e.getMessage());
+            }
+        }
 
     }
 
